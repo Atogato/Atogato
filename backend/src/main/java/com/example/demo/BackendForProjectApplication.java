234378c50069f4feff9ead29b78c2ee3bd36bcd2
9907daniel <daniel.9907@hotmail.com>
2023-06-12 20:37:10 +0900

    build(backend): 최초 재설치

--- backend/src/main/java/com/example/demo/BackendForProjectApplication.java
+++ backend/src/main/java/com/example/demo/BackendForProjectApplication.java
@@ -1,13 +1,13 @@
-package com.example.backend;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendForProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(BackendForProjectApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/example/demo/BackendForProjectApplicationTests.java
+++ backend/src/test/java/com/example/demo/BackendForProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backend;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class BackendForProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
