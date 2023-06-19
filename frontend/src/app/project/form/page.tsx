8255b61fee6823194428707bdc16176203f5580e
flashsoon <developsoon@gmail.com>
2023-06-19 11:23:30 +0900

    feat(fe): project-form-ui wireframe 제작

---
+++ frontend/src/app/project/form/page.tsx
@@ -0,0 +1,10 @@
+import ProjectFormContainer from './ProjectFormContainer'
+import ProjectForm from './ProjectForm'
+
+export default function ProjectFormPage() {
+  return (
+    <ProjectFormContainer>
+      <ProjectForm />
+    </ProjectFormContainer>
+  )
+}
