06dabcfcf0fefb0802ecd4c8d24946e455c20733
flashsoon <developsoon@gmail.com>
2023-06-29 20:18:13 +0900

    Chore(FE): .husky pre-commit yarn script 적용

--- frontend/src/app/project/list/components/Newproject.tsx
+++ frontend/src/app/project/list/components/Newproject.tsx
@@ -1,64 +1,69 @@
-export default function Newproject(){
-    const projects = [
-        {
-          id: 1,
-          projectName: 'Project 1',
-          poster: 'name1',
-          description: 'This is the description for Project 1',
-        },
-        {
-          id: 2,
-          projectName: 'Project 2',
-          poster: 'name2',
-          description: 'This is the description for Project 2',
-        },
-        {
-          id: 3,
-          projectName: 'Project 3',
-          poster: 'name3',
-          description: 'This is the description for Project 3',
-        },
-      ];
-    return(
-        <div className="max-w-5xl mx-auto">
-            <div className="text-2xl text-left my-2">
-                NEW
-            </div>
-            <div className="grid grid-cols-3 gap-4 place-content-center">
-            {projects.map((project)=>{
-                return(
-                    <div className="bg-cover bg-center h-80 relative" style={{ backgroundImage: `url('/images/sample/image.png')` }} key={project.id}>
-                        <div className="absolute bottom-0 md:p-3 text-center md:text-left space-y-2 inset-x-0">
-                            <div className="absolute top-0 left-0 w-full h-full bg-white opacity-70"></div>
-                            <div className="font-medium ">
-                                <div className="text-sky-500 dark:text-sky-400 mr-4 relative z-10">
-                                    {project.projectName}
-                                </div>
-                                <div className="text-sm text-slate-700 dark:text-slate-500 relative z-10">
-                                    {project.poster}
-                                </div>
-                            </div>
-                            <div className="absolute top-1 right-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-                                </svg> 
-                            </div>
-                            <p className="text-slate-700 text-sm dark:text-slate-500 relative z-10">
-                                {project.description}
-                            </p>
-                            <div className="text-sm text-slate-700 dark:text-slate-500 relative z-10">
-                                모집인원 분야
-                            </div>
-                            <div className="absolute bottom-2 right-2">
-                                <button className="text-sm bg-transparent hover:bg-blue-500 text-blue-700 hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded relative z-10">
-                                    더보기
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })}
+export default function Newproject() {
+  const projects = [
+    {
+      id: 1,
+      projectName: 'Project 1',
+      poster: 'name1',
+      description: 'This is the description for Project 1',
+    },
+    {
+      id: 2,
+      projectName: 'Project 2',
+      poster: 'name2',
+      description: 'This is the description for Project 2',
+    },
+    {
+      id: 3,
+      projectName: 'Project 3',
+      poster: 'name3',
+      description: 'This is the description for Project 3',
+    },
+  ]
+  return (
+    <div className="mx-auto max-w-5xl">
+      <div className="my-2 text-left text-2xl">NEW</div>
+      <div className="grid grid-cols-3 place-content-center gap-4">
+        {projects.map((project) => {
+          return (
+            <div
+              className="relative h-80 bg-cover bg-center"
+              style={{ backgroundImage: `url('/images/sample/image.png')` }}
+              key={project.id}
+            >
+              <div className="absolute inset-x-0 bottom-0 space-y-2 text-center md:p-3 md:text-left">
+                <div className="absolute left-0 top-0 h-full w-full bg-white opacity-70"></div>
+                <div className="font-medium ">
+                  <div className="relative z-10 mr-4 text-sky-500 dark:text-sky-400">{project.projectName}</div>
+                  <div className="relative z-10 text-sm text-slate-700 dark:text-slate-500">{project.poster}</div>
                 </div>
-        </div>
-    )
+                <div className="absolute right-2 top-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                    />
+                  </svg>
+                </div>
+                <p className="relative z-10 text-sm text-slate-700 dark:text-slate-500">{project.description}</p>
+                <div className="relative z-10 text-sm text-slate-700 dark:text-slate-500">모집인원 분야</div>
+                <div className="absolute bottom-2 right-2">
+                  <button className="relative z-10 rounded border border-blue-500 bg-transparent px-2 py-1 text-sm text-blue-700 hover:border-transparent hover:bg-blue-500 hover:text-white">
+                    더보기
+                  </button>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
--- frontend/src/app/project/list/page.tsx
+++ frontend/src/app/project/list/page.tsx
@@ -1,24 +1,19 @@
+import Newproject from './components/Newproject'
+import Timeline from './components/Timeline'
+import Filter from './components/Filter'
 
-import Newproject from "./components/Newproject"
-import Timeline from './components/Timeline';
-import Filter from "./components/Filter";
-
-export default function List(){
+export default function List() {
   return (
     <div>
-        <Newproject />
-        <div className="max-w-5xl mx-auto">
-          <div className="text-2xl text-left my-2">
-            Timeline
-          </div>
-          <Timeline />
-        </div>
-        <div className="max-w-5xl mx-auto my-5">
-            <div className="text-2xl text-left my-2">
-                Filters
-            </div>
-            <Filter />
-        </div>
+      <Newproject />
+      <div className="mx-auto max-w-5xl">
+        <div className="my-2 text-left text-2xl">Timeline</div>
+        <Timeline />
+      </div>
+      <div className="mx-auto my-5 max-w-5xl">
+        <div className="my-2 text-left text-2xl">Filters</div>
+        <Filter />
+      </div>
     </div>
-  );
-};
+  )
+}
