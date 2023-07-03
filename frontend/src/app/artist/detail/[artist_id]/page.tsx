43671cfcad5ca2f5dbe2fe182bf8fc7792b5531b
KimSoonYo <developSoon@gmail.com>
2023-07-03 17:03:14 +0900

    Feat(FE): artist detail ui 제작 (#23)

---
+++ frontend/src/app/artist/detail/[artist_id]/page.tsx
@@ -0,0 +1,9 @@
+import ArtistDetailContainer from './ArtistDetailContainer'
+import ArtistDetailContents from './ArtistDetailContents'
+export default function ArtistDetailPage() {
+  return (
+    <ArtistDetailContainer>
+      <ArtistDetailContents />
+    </ArtistDetailContainer>
+  )
+}
