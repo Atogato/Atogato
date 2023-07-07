e5294e10f84eb9bf27a3240e529ed429677d92c6
Bor Yun Heo <boryun@Bors-MacBook-Pro-2.local>
2023-07-07 21:35:24 +0900

    Feat(FE): navbar 링크 수정 & prettier

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className='relative'>
+      <body className="relative">
         <Navbar />
         {children}
         <Footer />
