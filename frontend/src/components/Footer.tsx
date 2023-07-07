2dc35bbe183863a71eac2d5b77ef994f47876ca9
Bor Yun Heo <boryun@Bors-MacBook-Pro-2.local>
2023-07-07 21:35:24 +0900

    Feat(FE): navbar 링크 수정 & prettier

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,30 +1,36 @@
-export default function Footer(){
-    return(
-        <div>
-            <footer className="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="#" target="_blank" className="flex items-center mb-4 sm:mb-0">
-                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Project</span>
-                    </a>
-                    <ul className="flex flex-wrap items-center mb-6 sm:mb-0">
-                        <li>
-                            <a href="#" className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400">About</a>
-                        </li>
-                        <li>
-                            <a href="#" className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400">Privacy
-                                Policy</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" className="text-sm text-gray-500 hover:underline dark:text-gray-400">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <hr className="my-2 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            </footer>
+export default function Footer() {
+  return (
+    <div>
+      <footer className="rounded-lg bg-white p-4 shadow dark:bg-gray-800 md:px-6 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <a href="#" target="_blank" className="mb-4 flex items-center sm:mb-0">
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Project</span>
+          </a>
+          <ul className="mb-6 flex flex-wrap items-center sm:mb-0">
+            <li>
+              <a href="#" className="mr-4 text-sm text-gray-500 hover:underline dark:text-gray-400 md:mr-6">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 text-sm text-gray-500 hover:underline dark:text-gray-400 md:mr-6">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 text-sm text-gray-500 hover:underline dark:text-gray-400 md:mr-6">
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-sm text-gray-500 hover:underline dark:text-gray-400">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-    )
+        <hr className="my-2 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
+      </footer>
+    </div>
+  )
 }
