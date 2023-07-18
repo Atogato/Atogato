f201fdb9f6a7cfcd95d244393a3127a1743e0dc6
Bor Yun Heo <boryun@Bors-MacBook-Pro-2.local>
2023-07-18 21:15:38 +0900

    Feat(FE): 프로젝트 신청자 ui 수정

---
+++ frontend/src/app/project/application/components/ProfileCard.tsx
@@ -0,0 +1,11 @@
+export default function ProfileCard() {
+  return (
+    <div className="rounded-1xl mx-4 my-1 grid grid-cols-4 bg-white">
+      <div className="col-span-1 m-2 flex items-center">
+        <img alt="name" src="/images/sample/image.png" className="h-10 w-10 rounded-full" />
+      </div>
+      <div className="col-span-2 flex items-center">Name</div>
+      <div className="col-span-1 m-2 flex items-center justify-center bg-gray-200">Profile</div>
+    </div>
+  )
+}
