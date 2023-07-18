18f5d0a4995c154d91f6608f19d6fecb0ba895b5
Bor Yun Heo <boryun@Bors-MacBook-Pro-2.local>
2023-07-18 21:09:55 +0900

    Feat(FE): 프로젝트 신청자 ui

---
+++ frontend/src/app/project/application/components/Information.tsx
@@ -0,0 +1,10 @@
+export default function Information() {
+  return (
+    <div className="bg-gray-200 text-center">
+      <div>프로젝트 정보</div>
+      <div>프로젝트 설명입니다 인원 날짜</div>
+      <div>인원</div>
+      <div>날짜</div>
+    </div>
+  )
+}
