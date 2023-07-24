868cfef2beae0b1fa05b025145c173dea03fd656
Bor Yun Heo <boryun@Bors-MacBook-Pro-2.local>
2023-07-24 21:48:29 +0900

    Feat(FE): Prettier

--- frontend/src/app/artist/list/components/FilteredList.tsx
+++ frontend/src/app/artist/list/components/FilteredList.tsx
@@ -1,4 +1,4 @@
-import ArtistCard from "./ArtistCard";
+import ArtistCard from './ArtistCard'
 import { Artists } from '../page'
 
 type filteredListProps = {
@@ -6,33 +6,26 @@ type filteredListProps = {
   artists: Artists[]
 }
 export default function Filteredlist({ options, artists }: filteredListProps): JSX.Element {
+  const filterbyAll = artists.filter(
+    (artist) => artist.location === options[0] && artist.creatorArtCategory === options[1],
+  )
+  const filterbyLocation = artists.filter((artist) => artist.location === options[0])
+  const filterbyCategory = artists.filter((artist) => artist.creatorArtCategory === options[1])
 
-    const filterbyAll = artists.filter(
-    (artist) => artist.location === options[0] && artist.creatorArtCategory === options[1] 
-    )
-    const filterbyLocation = artists.filter(
-    (artist) =>  artist.location === options[0] 
-    )
-    const filterbyCategory = artists.filter(
-    (artist) => artist.creatorArtCategory === options[1] 
-    )
-      
-    return(
-        <div>
-            <div className="text-2xl text-left my-2">
-                ARTISTS
-            </div>
-                <div className="grid grid-cols-3 gap-4 place-content-center">
-                {options[0] === "All" && options[1] === "All" ? (
-                artists.map((artist) => <ArtistCard artist={artist} key={artist.artistId}/>)
-                ) : options[0] !== "All" && options[1] !== "All" ? (
-                filterbyAll.map((artist) => <ArtistCard artist={artist} key={artist.artistId}/>)
-                ) : options[0] !== "All" && options[1] === "All"  ? (
-                filterbyLocation.map((artist) => <ArtistCard artist={artist} key={artist.artistId}/>)
-                ) : options[0] === "All" && options[1] !== "All" ? (
-                filterbyCategory.map((artist) => <ArtistCard artist={artist} key={artist.artistId}/>)
-                ) : null}
-                </div>
-        </div>
-        )   
+  return (
+    <div>
+      <div className="my-2 text-left text-2xl">ARTISTS</div>
+      <div className="grid grid-cols-3 place-content-center gap-4">
+        {options[0] === 'All' && options[1] === 'All'
+          ? artists.map((artist) => <ArtistCard artist={artist} key={artist.artistId} />)
+          : options[0] !== 'All' && options[1] !== 'All'
+          ? filterbyAll.map((artist) => <ArtistCard artist={artist} key={artist.artistId} />)
+          : options[0] !== 'All' && options[1] === 'All'
+          ? filterbyLocation.map((artist) => <ArtistCard artist={artist} key={artist.artistId} />)
+          : options[0] === 'All' && options[1] !== 'All'
+          ? filterbyCategory.map((artist) => <ArtistCard artist={artist} key={artist.artistId} />)
+          : null}
+      </div>
+    </div>
+  )
 }
