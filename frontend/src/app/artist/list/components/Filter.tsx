1bfe148540d1e3fd1ba50a69862cbc6039f0fb02
Bor Yun Heo <boryun@Bors-MacBook-Pro-2.local>
2023-07-24 21:45:15 +0900

    Feat(FE) : 프로젝트 연동 api temp

---
+++ frontend/src/app/artist/list/components/Filter.tsx
@@ -0,0 +1,86 @@
+
+'use client'
+import { useState } from 'react';
+import FilteredList from './FilteredList';
+import { Artists } from '../page'
+import { localStorage } from '@/app/storage'
+
+type artistsProps = {
+  data: Artists[]
+}
+
+export default function Filter({ data }: artistsProps): JSX.Element {
+  const token = localStorage.getItem('token');
+ 
+  console.log('Token found in localStorage:', token);
+
+  const [selectedOptions, setSelectedOptions] = useState<string[]>(["All", "All"]);
+  const [isActive, setIsActive] = useState([false, false]);
+
+  const handleOptionSelect = (filterIndex : number, option: string) => {
+    setSelectedOptions((prevOptions) => {
+      const updatedOptions = [...prevOptions];
+      updatedOptions[filterIndex] = option;
+      return updatedOptions;
+    });
+  };
+
+  const handleToggle = (index: number) => {
+    setIsActive((prevIsActive) => {
+      const updatedIsActive = [...prevIsActive];
+      updatedIsActive[index] = !updatedIsActive[index];
+      console.log(selectedOptions)
+
+      return updatedIsActive;
+    });
+  };
+
+  return (
+    <div>
+        <div className='flex'> 
+            <div className="relative mr-3">
+                <button
+                onClick= {() => {
+                    handleToggle(0);
+                }}
+                className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md focus:outline-none"
+                >
+                지역
+                </button>
+                {isActive[0] && (
+                <div className="absolute mt-2 w-20 bg-white shadow-lg rounded-md text-center">
+                    <ul className="py-2">
+                    <li onClick={() => handleOptionSelect(0 , 'All')} className="px-2 py-2 hover:bg-gray-100">All</li>
+                    <li onClick={() => handleOptionSelect(0 , '서울')} className="px-2 py-2 hover:bg-gray-100">서울</li>
+                    <li onClick={() => handleOptionSelect(0 , '경기')} className="px-2 py-2 hover:bg-gray-100">경기</li>
+                    <li onClick={() => handleOptionSelect(0 , '부산')} className="px-2 py-2 hover:bg-gray-100">부산</li>
+                    </ul>
+                </div>
+                )}
+            </div>
+            <div className="relative mr-3">
+                <button
+                onClick= {() => {
+                    handleToggle(1);
+                }}
+                className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md focus:outline-none"
+                >
+                장르
+                </button>
+                {isActive[1] && (
+                <div className="absolute mt-2 w-20 bg-white shadow-lg rounded-md text-center">
+                    <ul className="py-2">
+                    <li onClick={() => handleOptionSelect(1 , 'All')}className="px-2 py-2 hover:bg-gray-100">All</li>
+                    <li onClick={() => handleOptionSelect(1 , 'Option 1')}className="px-2 py-2 hover:bg-gray-100">Option 1</li>
+                    <li onClick={() => handleOptionSelect(1, 'Option 2')}className="px-2 py-2 hover:bg-gray-100">Option 2</li>
+                    <li onClick={() => handleOptionSelect(1, 'Option 3')}className="px-2 py-2 hover:bg-gray-100">Option 3</li>
+                    </ul>
+                </div>
+                )}
+            </div>
+        </div>    
+        <FilteredList options={selectedOptions} artists={data} />
+    </div>
+  );
+};
+
