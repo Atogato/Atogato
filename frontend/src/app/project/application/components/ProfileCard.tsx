08ebc2eaace424abbb844a0450a94c798a786a6f
Bor Yun Heo <boryun@Bors-MacBook-Pro-2.local>
2023-07-27 16:36:30 +0900

    Feat(FE): 신청자 관리 페이지 수정

--- frontend/src/app/project/application/components/ProfileCard.tsx
+++ frontend/src/app/project/application/components/ProfileCard.tsx
@@ -1,11 +1,23 @@
-export default function ProfileCard() {
+import Image from 'next/image'
+
+type MemberType = {
+  id: number
+  userName: string
+}
+interface MemberProps {
+  member: MemberType
+}
+
+export default function ProfileCard({ member }: MemberProps): JSX.Element {
   return (
     <div className="rounded-1xl mx-4 my-1 grid grid-cols-4 bg-white">
       <div className="col-span-1 m-2 flex items-center">
-        <img alt="name" src="/images/sample/image.png" className="h-10 w-10 rounded-full" />
+        <Image width={400} height={400} alt="name" src="/images/sample/image.png" className="h-10 w-10 rounded-full" />
       </div>
-      <div className="col-span-2 flex items-center">Name</div>
-      <div className="col-span-1 m-2 flex items-center justify-center bg-gray-200">Profile</div>
+      <div className="col-span-2 flex items-center">{member.userName}</div>
+      <button className="col-span-1 m-2 flex items-center justify-center bg-gray-200">
+        <div>Profile</div>
+      </button>
     </div>
   )
 }
