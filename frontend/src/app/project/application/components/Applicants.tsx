dc74ed8d3bc485ff68801f8e8ab81bc772cfcfac
Bor Yun Heo <boryun@Bors-MacBook-Pro-2.local>
2023-07-27 16:36:30 +0900

    Feat(FE): 신청자 관리 페이지 수정

--- frontend/src/app/project/application/components/Applicants.tsx
+++ frontend/src/app/project/application/components/Applicants.tsx
@@ -1,15 +1,30 @@
 import ApplicationCard from './ApplicationCard'
 
 export default function Applicants() {
+  const messages = [
+    {
+      id: 1,
+      userName: 'name 1',
+      chat: 'message 1',
+    },
+    {
+      id: 2,
+      userName: 'name 2',
+      chat: 'message 2',
+    },
+    {
+      id: 3,
+      userName: 'name 3',
+      chat: 'message 3',
+    },
+  ]
   return (
     <div className="bg-gray-200 p-2">
       <div className="p-2">신청자</div>
       <div className="h-56 overflow-auto">
-        <ApplicationCard />
-        <ApplicationCard />
-        <ApplicationCard />
-        <ApplicationCard />
-        <ApplicationCard />
+        {messages.map((message) => (
+          <ApplicationCard message={message} key={message.id} />
+        ))}
       </div>
     </div>
   )
