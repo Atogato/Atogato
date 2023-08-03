c873080a177015d64b2747d0fbca8b3d1ff20d7c
flashsoon <developsoon@gmail.com>
2023-08-03 10:36:30 +0900

    Chore(FE): Form app routing 구조 변경

--- frontend/src/app/(form)/[slug]/form/page.tsx
+++ frontend/src/app/(form)/[slug]/form/page.tsx
@@ -1,10 +1,9 @@
 import { notFound } from 'next/navigation'
 import { Form } from '@/components/form/Form'
-import { FormType } from '@/types/form'
 
-export const FORM_TYPES: FormType[] = ['artist', 'project', 'signup', 'login']
+export const FORM_TYPES = ['artist', 'project', 'signup', 'login']
 
-export default function FormPage({ params }: { params: { slug: FormType } }) {
+export default function FormPage({ params }: { params: { slug: 'artist' | 'project' | 'signup' | 'login' } }) {
   const { slug } = params
   if (!FORM_TYPES.includes(slug)) {
     notFound()
