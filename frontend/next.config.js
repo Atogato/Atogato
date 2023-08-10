59dbcd627d1a871da7ebc23b40a7bcdd84f78ad5
flashsoon <developsoon@gmail.com>
2023-08-10 22:36:44 +0900

    Design(FE): NavBar 디자인 적용

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
   env: {
     BACKEND_API_URL: process.env.BACKEND_API_URL,
     BACKEND_OAUTH_URL: process.env.BACKEND_OAUTH_URL,
