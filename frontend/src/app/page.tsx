89f8b611e0f7ec64b1b378a886fd580f7900c19d
flashsoon <developsoon@gmail.com>
2023-08-25 00:11:46 +0900

    Style(FE): 랜덤 프로젝트 Slider 제작

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,16 @@
 import Banner from '@/components/Banner'
 import ProjectSection from '@/components/ProjectSection'
+import RandomSection from '@/components/RandomSection'
+
 export default function HomePage() {
   return (
     <main>
       <Banner className="min-h-fit" />
       <section>
         <ProjectSection />
+      </section>
+      <section>
+        <RandomSection />
       </section>
     </main>
   )
