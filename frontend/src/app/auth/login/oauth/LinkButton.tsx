049167659e1d65a1f20b4c10c74382d2f53433e3
9907daniel <daniel.9907@hotmail.com>
2024-01-31 23:39:08 +0900

    changes to oauth redirect

--- frontend/src/app/auth/login/oauth/LinkButton.tsx
+++ frontend/src/app/auth/login/oauth/LinkButton.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 export default function LinkButton({ socialType }: { socialType: string }) {
-  const oauth_uri = process.env.BACKEND_OAUTH_URL + `${socialType}?redirect_uri=${process.env.OAUTH_REDIRECT_URL}`
+  const oauth_uri = process.env.BACKEND_OAUTH_URL + `/api/${socialType}?redirect_uri=${process.env.OAUTH_REDIRECT_URL}`
   return (
     <a href={oauth_uri} className="border-2 hover:bg-gray-500">
       {socialType}
